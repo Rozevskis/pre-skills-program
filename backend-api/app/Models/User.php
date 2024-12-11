a9193141edd8c0d9323678078096fbf25fdfad34
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-12-11 20:51:34 +0200

    Ticket model, migration, factory, seeder

--- backend-api/app/Models/User.php
+++ backend-api/app/Models/User.php
@@ -44,6 +44,15 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'isAdmin' => 'boolean'
         ];
+    }
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
