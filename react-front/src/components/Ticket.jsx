efa9a4564c6f000bd24f9aabbfa08fa30a73bd0d
Kristofers Jēkabs Rozevskis <Kristofers.rozevskis@gmail.com>
2024-12-12 16:14:50 +0200

    Options menu opens

--- react-front/src/components/Ticket.jsx
+++ react-front/src/components/Ticket.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 export default function Ticekt({ id, title, details }) {
+  const [options, setOptions] = useState(false);
+
+  function handleOptions() {
+    if (!options) {
+      setOptions(true);
+    } else {
+      setOptions(false);
+    }
+  }
   return (
     <>
-      <div className="p-3 gap-2 bg-neutral-100 rounded-xl text-left flex flex-col">
-        <p>{id}</p>
+      <div className="p-3 gap-2 bg-neutral-100 rounded-xl text-left flex flex-col shadow-md">
+        <div className="flex justify-between">
+          {id}{" "}
+          <a onClick={() => handleOptions()} className=" cursor-pointer">
+            ...
+          </a>
+          <div
+            className={`
+                ${!options ? "hidden" : ""}
+                mt-[30px] 
+            ml-[180px] absolute p-3 gap-2 bg-slate-200 rounded-xl text-left flex flex-col shadow-md `}
+          >
+            <a>Delete</a>
+            <a>Edit</a>
+            {/* <a >History</a> */}
+          </div>
+        </div>
         <p>{title}</p>
         {/* <p>{details}</p> */}
       </div>
